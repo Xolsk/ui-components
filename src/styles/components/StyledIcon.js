87a559c1730bd03892e9723312d17f8d9dac3a7b
Matias Menker <menkermatias@gmail.com>
2019-12-06 02:05:30 -0300

    storybook of release 2.0.0

--- src/styles/components/StyledIcon.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const StyledIcon = styled.svg`
-  fill: ${props => props.fillColor};
-`;
-
-export { StyledIcon };
--- src/styles/components/StyledParagraph.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const StyledParagraph = styled.p`
-  margin: ${props => props.margin};
-  color: #404145;
-  font-size: ${props => props.size};
-`;
-
-export { StyledParagraph };
